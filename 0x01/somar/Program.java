0c8c1cb3dc60525946adf11211e937654d3a46e4
Igor147 <igorsilvasf4@gmail.com>
2022-05-05 09:30:36 -0300

    bug fixed

--- 0x01/somar/Program.java
+++ 0x01/somar/Program.java
@@ -1,4 +1,4 @@
-ublic class Program {
+public class Program {
     public static void main(String[] args) {
         System.out.printf("%d\n", Numero.somar(24, 32));
         System.out.printf("%d\n", Numero.somar(5, 0));
