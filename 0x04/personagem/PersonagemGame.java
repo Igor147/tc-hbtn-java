19a50185a7d4ff430e4157769cb61d5090e86215
Igor147 <igorsilvasf4@gmail.com>
2022-05-11 09:08:23 -0300

    game done

---
+++ 0x04/personagem/PersonagemGame.java
@@ -0,0 +1,10 @@
+public class PersonagemGame {
+    public int saudeAtual;
+    public String nome;
+}   if (getSaudeAtual() + quantidadeDeCura <= 100){
+            setSaudeAtual(getSaudeAtual() + quantidadeDeCura);
+        } else {
+            setSaudeAtual(100);
+        }
+    }
+}
