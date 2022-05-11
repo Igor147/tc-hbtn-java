3efe2cbb0687f36d388b04226f1c6e5b3a58a4e2
Igor147 <igorsilvasf4@gmail.com>
2022-05-11 08:54:43 -0300

    contas bancarias done

--- 0x03/conta_basica/Program.java
+++ 0x03/conta_basica/Program.java
@@ -1,55 +1,5 @@
 public class Program {
     public static void main(String[] args) {
-
-        try {
-            Livro livro1 = new Livro(
-                    "As barbas do imperador: D. Pedro II",
-                    "Lilia Moritz Schwarcz",
-                    56);
-
-            System.out.printf(java.util.Locale.GERMANY,"Titulo: %s - Autor: %s - Preco: %.2f\n",
-                    livro1.getTitulo(),
-                    livro1.getAutor(),
-                    livro1.getPreco());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
-
-        try {
-            LivroEdicaoOuro livroEdicaoOuro1 = new LivroEdicaoOuro(
-                    "Xato",
-                    "Fernando Morais",
-                    38.30);
-
-            System.out.printf(java.util.Locale.GERMANY, "Titulo: %s - Autor: %s - Preco: %.2f\n",
-                    livroEdicaoOuro1.getTitulo(),
-                    livroEdicaoOuro1.getAutor(),
-                    livroEdicaoOuro1.getPreco());
-
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
-
-        try {
-            LivroEdicaoOuro livroEdicaoOuro2 = new LivroEdicaoOuro(
-                    "Ladrao de Raios",
-                    "Rick",
-                    21.88);
-
-            System.out.printf(java.util.Locale.GERMANY, "Titulo: %s - Autor: %s - Preco: %.2f\n",
-                    livroEdicaoOuro2.getTitulo(),
-                    livroEdicaoOuro2.getAutor(),
-                    livroEdicaoOuro2.getPreco());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
-
-        try {
-            Livro livro2 = new Livro(
-                    "ab",
-                    "Jane Doe",
-           public class Program {
-    public static void main(String[] args) {
         try {
             ContaBancariaBasica conta1 = new ContaBancariaBasica("AAA", 50.0);
             System.out.printf(java.util.Locale.GERMANY,"Conta %s - saldo: %.2f\n", conta1.getNumeracao(), conta1.getSaldo());
@@ -101,30 +51,6 @@ public class Program {
             conta5.depositar(25);
             System.out.printf(java.util.Locale.GERMANY,"Conta %s - saldo: %.2f\n", conta5.getNumeracao(), conta5.getSaldo());
             conta5.sacar(30);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
-    }
-}         15.99);
-
-            System.out.printf(java.util.Locale.GERMANY, "Titulo: %s - Autor: %s - Preco: %.2f\n",
-                    livro2.getTitulo(),
-                    livro2.getAutor(),
-                    livro2.getPreco());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
-
-        try {
-            Livro livro3 = new Livro(
-                    "Harry Potter e o enigma do principe",
-                    "J.K. Rowling",
-                    0);
-
-            System.out.printf(java.util.Locale.GERMANY, "Titulo: %s - Autor: %s - Preco: %.2f\n",
-                    livro3.getTitulo(),
-                    livro3.getAutor(),
-                    livro3.getPreco());
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
