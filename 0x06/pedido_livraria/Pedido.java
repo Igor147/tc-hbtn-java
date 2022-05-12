1d197a8764d2b3f6df9238018a8bb7e9f7de7849
Igor147 <igorsilvasf4@gmail.com>
2022-05-12 17:55:52 -0300

    heranca fixacao done

---
+++ 0x06/pedido_livraria/Pedido.java
@@ -0,0 +1,25 @@
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public double calcularTotal() {
+        double somaTotal = 0;
+        for (ItemPedido item : getItens()){
+            somaTotal = somaTotal + item.getProduto().obterPrecoLiquido() * item.getQuantidade();
+        }
+        return somaTotal * (1 - getPercentualDesconto()/100);
+    }
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public ItemPedido[] getItens() {
+        return itens;
+    }
+}
