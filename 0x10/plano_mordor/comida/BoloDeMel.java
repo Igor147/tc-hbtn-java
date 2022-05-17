cffe32415b55a164bdcde7cd6cc5b14c370350eb
Igor147 <igorsilvasf4@gmail.com>
2022-05-17 08:43:42 -0300

    OO done

---
+++ 0x10/plano_mordor/comida/BoloDeMel.java
@@ -0,0 +1,13 @@
+packapublic class PrintFloatVariable {
+    public static void main(String[] args) {
+        float pi = 3.14159f;
+        System.out.printf("Valor de pi: %.3f\n", pi);
+    }
+}ge comida;
+
+public class BoloDeMel extends Comida{
+
+    public BoloDeMel() {
+        super(5);
+    }
+}
